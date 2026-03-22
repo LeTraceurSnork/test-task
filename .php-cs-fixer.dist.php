b63f913bb197f45c1ceae88c18a9c447a5062409
LeTraceurSnork <le_traceur_snork@mail.ru>
2026-03-22 19:39:19 +0500

    fix: csfix

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,9 +5,10 @@ $finder = new PhpCsFixer\Finder()
 
 return new PhpCsFixer\Config()
     ->setRules([
-        '@PER-CS'                => true,
-        '@PHP8x4Migration'       => true,
-        'binary_operator_spaces' => [
+        '@PER-CS'                      => true,
+        '@PHP8x4Migration'             => true,
+        'blank_line_after_opening_tag' => false,
+        'binary_operator_spaces'       => [
             'default'   => 'align_single_space_minimal',
             'operators' => [
                 '='  => 'align_single_space',
@@ -15,5 +16,8 @@ return new PhpCsFixer\Config()
                 '??' => 'single_space',
             ],
         ],
+        'cast_spaces'                  => false,
+        'no_blank_lines_after_phpdoc'  => false,
+        'single_line_empty_body'       => false,
     ])
     ->setFinder($finder);
